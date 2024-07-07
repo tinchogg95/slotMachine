1d545a6691c714b26ae20d5ca47b57e509e6bde5
gimma01 <martin.gimenez@trp.com.ar>
2024-07-07 02:11:33 -0300

    revert and new changes

--- src/globals.js
+++ src/globals.js
@@ -2,15 +2,13 @@ const globals = {
     imageObjects: [],
     imageObjects2: [],
     imageObjects3: [],
-    totalImages: 500,
-    imageSpacing: 200,
     scrollSpeed: 25,
     isButtonPressed: false,
     rabbitMoney: null,
     textWinner: null,
-    spinButton: null,
     restartButton: null,
-    sadFace: null,
+    spinButton: null,
+    sadFace: null
 };
 
 export default globals;
